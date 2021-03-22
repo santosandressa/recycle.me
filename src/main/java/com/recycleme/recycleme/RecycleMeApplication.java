521a949579f62be93751b6c8de4421f838722717
Andressa Santos <andressasantosp0@gmail.com>
2021-03-22 12:53:10 -0300

    commit inicial

---
+++ src/main/java/com/recycleme/recycleme/RecycleMeApplication.java
@@ -0,0 +1,13 @@
+package com.recycleme.recycleme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recycleme/recycleme/RecycleMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recycleme.recycleme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
