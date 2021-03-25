d6a913f74377a35596e76d2b98fc8bc881f53696
Paula <paulacandidoalves@gmail.com>
2021-03-25 14:31:01 -0300

    Post, id, cnpj Usuario

--- src/main/java/com/recycleme/recycleme/controller/UsuarioController.java
+++ src/main/java/com/recycleme/recycleme/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +39,23 @@ public class UsuarioController {
 	public void delete (@PathVariable Long id) 
 	{
 		repository.deleteById(id);
+	}
+	
+	//id 
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	//CNPJ 
+	@GetMapping("/cnpj/{cnpj}")
+	public ResponseEntity<List<Usuario>> GetByCnpj(@PathVariable String cnpj){
+		return ResponseEntity.ok(repository.findAllByCnpjContainingIgnoreCase(cnpj));
+	}
+	
+	//POST
+	@PostMapping
+	public ResponseEntity<Usuario> PostUsuario (@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
 	}
 }
