ecd8a3647ad087053f538739bde65e147ed3d6f9
Andressa Santos <andressasantosp0@gmail.com>
2021-03-26 09:41:15 -0300

    get usuario

--- src/main/java/com/recycleme/recycleme/controller/UsuarioController.java
+++ src/main/java/com/recycleme/recycleme/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,43 +20,44 @@ import com.recycleme.recycleme.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/api/v1/recycleMe/usuario")
+@CrossOrigin("*")
 public class UsuarioController {
-	
-	@Autowired 
+
+	@Autowired
 	private UsuarioRepository repository;
-	
-	@GetMapping("/username/{username}")
-	public ResponseEntity<List<Usuario>> getByUsername(@PathVariable String username)
-	{
-		return ResponseEntity.ok(repository.findAllByUsernameContainingIgnoreCase(username));
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
 	}
-	
-	@PutMapping
-	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
-	}
-	
-	@DeleteMapping("/{id}")
-	public void delete (@PathVariable Long id) 
-	{
-		repository.deleteById(id);
-	}
-	
-	//id 
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
-	
-	//CNPJ 
+
 	@GetMapping("/cnpj/{cnpj}")
-	public ResponseEntity<List<Usuario>> GetByCnpj(@PathVariable String cnpj){
+	public ResponseEntity<List<Usuario>> GetByCnpj(@PathVariable String cnpj) {
 		return ResponseEntity.ok(repository.findAllByCnpjContainingIgnoreCase(cnpj));
 	}
-	
-	//POST
+
+	@GetMapping("/username/{username}")
+	public ResponseEntity<List<Usuario>> getByUsername(@PathVariable String username) {
+		return ResponseEntity.ok(repository.findAllByUsernameContainingIgnoreCase(username));
+	}
+
 	@PostMapping
-	public ResponseEntity<Usuario> PostUsuario (@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> PostUsuario(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
