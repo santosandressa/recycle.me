fd83a3cd1ba498f54e745804f8903fb78aac34d4
ySodias <ySodia@gmail.com>
2021-04-09 10:26:54 -0300

    Adicionando Repositories a UsuarioService

--- src/main/java/com/recycleme/recycleme/service/UsuarioService.java
+++ src/main/java/com/recycleme/recycleme/service/UsuarioService.java
@@ -1,14 +1,36 @@
 package com.recycleme.recycleme.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.recycleme.recycleme.model.Produto;
+import com.recycleme.recycleme.repository.AvaliacaoRepository;
+import com.recycleme.recycleme.repository.ProdutoRepository;
+import com.recycleme.recycleme.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService{
+	
+	@Autowired
+	private UsuarioRepository reporitoryUsuario;
+	
+	@Autowired
+	private ProdutoRepository repositoryProduto;
+	
+	@Autowired
+	private AvaliacaoRepository avalicaoRepository;
+	
+	
+	
 	//cadastrar usuario
 	//logar
 
 	//cadastrar produto
 
+	public Produto cadastrarProduto(Produto novoProduto, Long idUsuario) {
+		Produto produtoExistente = repositoryProduto.save(novoProduto);
+	}
+	
 	//comprar
 
 	//cadastrar avalicao
